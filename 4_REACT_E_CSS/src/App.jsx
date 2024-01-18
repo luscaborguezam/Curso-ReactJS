284a9926178d68c00528d86672d2942ae30650a7
Lucas Borguezam <85205753+luscaborguezam@users.noreply.github.com>
2024-01-18 18:33:58 -0300

    08/01/2024
    Terminando videos do módulo 4
    Falta o desafio

--- 4_REACT_E_CSS/src/App.jsx
+++ 4_REACT_E_CSS/src/App.jsx
@@ -8,10 +8,14 @@ import './components/MyComponent.css'
 
 //components
 import MyComponent from './components/MyComponent'
+import Title from './components/Title'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const n = 15
+    const [parImpar, setParImpar] = useState(true)
+    const [redTitle, setRedTile] = useState(true)
+    const handleParImpar = () => {parImpar? setParImpar(false) : setParImpar(true)}
+    const handledTitle = () => {redTitle? setRedTile(false) : setRedTile(true)}
   return (
     <>
     {/* CSS global */}
@@ -21,7 +25,18 @@ function App() {
       <p>Paragafo App.js</p>
     {/* Inline CSS */}
       <p style={{color:'purple', padding:'25px', borderTop: '2px solid red'}}>Este elemento foi estilizado de forma inline</p>
-      
+    {/* CSS iinline dinãmico */}
+      <p style={n < 10? ({color:'palegoldenrod'}) : ({color: 'pink'})}>CSS dinãmico</p>
+      <p style={parImpar? ({color:'Highlight', backgroundColor:'whitesmoke'}) : ({color: 'pink', backgroundColor:'beige'})}
+        onClick={handleParImpar}
+      >CSS dinamico (Clique Aqui!)</p>
+    {/* Classe dinamica  */}
+      <h2 className={redTitle? ('red-title') : ('title')}
+        onClick={handledTitle}
+      >Titulo classe dinamica (click)</h2>
+    {/* CSS Module */}
+      <Title/>
+      <h1 className='my-title'>TESTANDO</h1>
     </>
   )
 }
